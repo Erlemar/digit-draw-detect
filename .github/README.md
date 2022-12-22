ca67f90997e63e665d951850a8c37f8566775102
Andrey <and-lukyane@yandex.ru>
2022-12-22 12:04:32 +0400

    Update descriptions. (#13)

--- .github/README.md
+++ .github/README.md
@@ -1,2 +1,13 @@
 ![visitors](https://visitor-badge.glitch.me/badge?page_id=wissamantoun.arabicnlpapp)
 [![DeepSource](https://static.deepsource.io/deepsource-badge-light-mini.svg)](https://deepsource.io/gh/Erlemar/digit-draw-detect/?ref=repository-badge )
+
+This is a repo of my "Handwritten digit detector" pet-project. It uses a YOLOv3 model trained from scratch and Streamlit for frontent. You can see the live version of the app [here](https://huggingface.co/spaces/Artgor/digit-draw-detect).
+
+If you are interested in reading more about this project, here are some links:
+* [Project page on my personal website](https://andlukyane.com/project/drawn-digits-prediction)
+* [A dataset with the digits and bounding boxes on Kaggle](https://www.kaggle.com/datasets/artgor/handwritten-digits-and-bounding-boxes)
+* [Training code](https://github.com/Erlemar/pytorch_tempest_pet_)
+* Blogpost on my personal website
+* [Blogpost on medium](https://towardsdatascience.com/the-third-life-of-a-personal-pet-project-for-handwritten-digit-recognition-fd908dc8e7a1)
+* Russian blogpost on habrahabr
+* [W&B report](https://wandb.ai/al-3002-w/pet_project_object_detection/reports/Training-a-model-for-Handwritten-Object-Detection---VmlldzozMTgwMzA2?accessToken=yi6t4sz6iwr1yp78nfpvw71qao5wibak30np9tfft885tdj26g3tk91h1sie3h5m)
--- pages/about.py
+++ pages/about.py
@@ -6,8 +6,8 @@ The first idea of the project was conceived in the summer of 2017. I completed c
 
 In 2019 I decided to update the [project](https://github.com/Erlemar/digit-draw-predict): I trained the new neural net in PyTorch and used cv2 to detect separate digits (people often drew multiple digits). More than that, the model had 11 classes - I made a separate class for "junk", as people often drew things for fun: animals, objects, or words.
 
-The first two versions were deployed on Heroku's free plan, but in 2022 these plans were discontinued. I didn't want my project to die because of nostalgia, so I developed a new version and deployed it differently. The current version has an object detection model (yolo3 written from scratch) and 12 classes (digits, junk, and **censored**)
-. If you want to know what does **censored** means, just try to draw something 😉
+The first two versions were deployed on Heroku's free plan, but in 2022 these plans were discontinued. I didn't want my project to die because of nostalgia, so I developed a new version and deployed it differently. The current version has an object detection model (yolo3 written from scratch) and 12 classes (digits, junk, and **censored**).
+If you want to know what does **censored** means, just try to draw something 😉
 
 Initially, I considered deploying the app on Streamlit Cloud, but its computational limits were too low, so now the model is live on HuggingFace Spaces.
 
@@ -16,7 +16,10 @@ Initially, I considered deploying the app on Streamlit Cloud, but its computatio
 * [Project page on my personal website](https://andlukyane.com/project/drawn-digits-prediction)
 * [A dataset with the digits and bounding boxes on Kaggle](https://www.kaggle.com/datasets/artgor/handwritten-digits-and-bounding-boxes)
 * [Training code](https://github.com/Erlemar/pytorch_tempest_pet_)
-
+* Blogpost on my personal website
+* [Blogpost on medium](https://towardsdatascience.com/the-third-life-of-a-personal-pet-project-for-handwritten-digit-recognition-fd908dc8e7a1)
+* Russian blogpost on habrahabr
+* [Project code on GitHub](https://github.com/Erlemar/digit-draw-detect)
 """
 
 st.markdown(text, unsafe_allow_html=True)
