5457bf5a3552dcc8142ee8abe26e2e5c1396523a
Andrey <and-lukyane@yandex.ru>
2023-09-02 07:43:20 +0400

    Update formatting.

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -88,17 +88,10 @@ def non_max_suppression(
     while bboxes:
         chosen_box = bboxes.pop(0)
 
-        bboxes = [
-            box
-            for box in bboxes
-            if box[0] != chosen_box[0]
-               or intersection_over_union(
-                torch.tensor(chosen_box[2:]),
-                torch.tensor(box[2:]),
-                box_format=box_format,
-            )
-               < iou_threshold
-        ]
+        bboxes = [box for box in bboxes if
+                  box[0] != chosen_box[0] or intersection_over_union(torch.tensor(chosen_box[2:]),
+                                                                     torch.tensor(box[2:]),
+                                                                     box_format=box_format, ) < iou_threshold]
 
         bboxes_after_nms.append(chosen_box)
 
